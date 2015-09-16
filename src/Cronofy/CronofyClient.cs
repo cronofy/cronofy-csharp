25b8df39a61f488a67b7bbcc66d49b4a45e3f325
Garry Shutler <garry@robustsoftware.co.uk>
2015-09-16 10:51:55 +0100

    Initial commit

---
+++ src/Cronofy/CronofyClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cronofy
+{
+	public sealed class CronofyClient
+	{
+		public CronofyClient()
+		{
+		}
+
+		public bool SmokeTest()
+		{
+			return true;
+		}
+	}
+}
---
+++ test/Cronofy.Test/SmokeTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Cronofy.Test
+{
+	[TestFixture()]
+	public class SmokeTest
+	{
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
