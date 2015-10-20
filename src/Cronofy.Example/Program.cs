3207ba669863b9c8e4559072e79a2ad32fa92308
Garry Shutler <garry@robustsoftware.co.uk>
2015-10-20 16:53:05 +0100

    Basic example console app

---
+++ src/Cronofy.Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cronofy.Example
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter access token: ");
+			var accessToken = Console.ReadLine();
+
+			Console.WriteLine();
+			var client = new CronofyAccountClient(accessToken);
+
+			Console.WriteLine("Fetching events...");
+			var events = client.GetEvents();
+
+			Console.WriteLine();
+
+			foreach (var evt in events)
+			{
+				Console.WriteLine("{0} - {1}", evt.Start, evt.Summary);
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Press enter to continue...");
+			Console.ReadLine();
+		}
+	}
+}
