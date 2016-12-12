815c71e39fbd7d50824b6da7b89d0204f24aee93
Harry Boyes <harry.boyes@gmail.com>
2016-12-12 13:01:09 +0000

    Implement user info call

---
+++ src/Cronofy/Responses/UserInfoResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace Cronofy.Responses
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Class for the deserialization of a user info response.
+    /// </summary>
+    internal sealed class UserInfoResponse
+    {
+        /// <summary>
+        /// Gets or sets the Cronofy account ID of the response.
+        /// </summary>
+        [JsonProperty("sub")]
+        public string Sub { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cronofy account type of the response.
+        /// </summary>
+        [JsonProperty("cronofy.type")]
+        public string CronofyType { get; set; }
+
+        /// <summary>
+        /// Converts the response into a <see cref="Cronofy.UserInfo"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Cronofy.UserInfo"/> based upon the response.
+        /// </returns>
+        public UserInfo ToUserInfo()
+        {
+            return new UserInfo
+            {
+                Sub = this.Sub,
+                CronofyType = this.CronofyType,
+            };
+        }
+    }
+}
