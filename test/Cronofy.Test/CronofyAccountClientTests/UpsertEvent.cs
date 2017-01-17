352d035983fa84f7000fe399645ea54aaa72fe88
Harry Boyes <harry.boyes@gmail.com>
2017-01-17 15:35:40 +0000

    Add geo location to upsert events

--- test/Cronofy.Test/CronofyAccountClientTests/UpsertEvent.cs
+++ test/Cronofy.Test/CronofyAccountClientTests/UpsertEvent.cs
@@ -133,6 +133,53 @@ namespace Cronofy.Test.CronofyAccountClientTests
         }
 
         [Test]
+        public void CanUpsertWithGeoLocation()
+        {
+            const string eventId = "qTtZdczOccgaPncGJaCiLg";
+            const string summary = "Board meeting";
+            const string description = "Discuss plans for the next quarter";
+            const string startTimeString = "2014-08-05 15:30:00Z";
+            const string endTimeString = "2014-08-05 17:00:00Z";
+            const string locationDescription = "Board room";
+            const string locationLatitude = "1.2345";
+            const string locationLongitude = "0.1234";
+
+            Http.Stub(
+                HttpPost
+                .Url("https://api.cronofy.com/v1/calendars/" + calendarId + "/events")
+                .RequestHeader("Authorization", "Bearer " + AccessToken)
+                .RequestHeader("Content-Type", "application/json; charset=utf-8")
+                .RequestBodyFormat(
+                    "{{\"event_id\":\"{0}\"," +
+                    "\"summary\":\"{1}\"," +
+                    "\"description\":\"{2}\"," +
+                    "\"start\":{{\"time\":\"{3}\",\"tzid\":\"Etc/UTC\"}}," +
+                    "\"end\":{{\"time\":\"{4}\",\"tzid\":\"Etc/UTC\"}}," +
+                    "\"location\":{{\"description\":\"{5}\",\"lat\":\"{6}\",\"long\":\"{7}\"}}" +
+                    "}}",
+                    eventId,
+                    summary,
+                    description,
+                    startTimeString,
+                    endTimeString,
+                    locationDescription,
+                    locationLatitude,
+                    locationLongitude)
+                .ResponseCode(202)
+            );
+
+            var builder = new UpsertEventRequestBuilder()
+                .EventId(eventId)
+                .Summary(summary)
+                .Description(description)
+                .Start(new DateTime(2014, 8, 5, 15, 30, 0, DateTimeKind.Utc))
+                .End(new DateTime(2014, 8, 5, 17, 0, 0, DateTimeKind.Utc))
+                .Location(locationDescription, locationLatitude, locationLongitude);
+
+            Client.UpsertEvent(calendarId, builder);
+        }
+
+        [Test]
         public void CanUpsertEventWithTimeZoneId()
         {
             const string eventId = "qTtZdczOccgaPncGJaCiLg";
