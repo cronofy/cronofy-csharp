39da4333cc25c40cce7e67c2f0bab89e95c5a425
Stephen Binns <stephen.binns@gmail.com>
2018-04-09 12:25:44 +0100

    Add explicit tests for event_private read and set

--- test/Cronofy.Test/CronofyAccountClientTests/GetEvents.cs
+++ test/Cronofy.Test/CronofyAccountClientTests/GetEvents.cs
@@ -60,7 +60,6 @@ namespace Cronofy.Test.CronofyAccountClientTests
       ],
       ""created"": ""2014-09-01T08:00:01Z"",
       ""updated"": ""2014-09-01T09:24:16Z"",
-      ""event_private"": true,
       ""options"": {
         ""delete"": true,
         ""update"": true
@@ -102,7 +101,6 @@ namespace Cronofy.Test.CronofyAccountClientTests
                             Delete = true,
                             Update = true
                         },
-                        EventPrivate = true,
                     }
                 },
                 events);
@@ -654,6 +652,92 @@ namespace Cronofy.Test.CronofyAccountClientTests
             AssertParameter(
                 expectedKeyValue,
                 b => b.CalendarIds(calendarIds.ToArray()));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void CanGetPrivateEvents(bool eventPrivate)
+        {
+            Http.Stub(
+                HttpGet
+                .Url("https://api.cronofy.com/v1/events?tzid=Etc%2FUTC&localized_times=true")
+                .RequestHeader("Authorization", "Bearer " + AccessToken)
+                .ResponseCode(200)
+                .ResponseBody(
+string.Format(@"{{
+  ""pages"": {{
+    ""current"": 1,
+    ""total"": 1
+  }},
+  ""events"": [
+    {{
+      ""calendar_id"": ""cal_U9uuErStTG@EAAAB_IsAsykA2DBTWqQTf-f0kJw"",
+      ""event_uid"": ""evt_external_54008b1a4a41730f8d5c6037"",
+      ""summary"": ""Company Retreat"",
+      ""description"": ""Escape to the country"",
+      ""start"": ""2014-09-06"",
+      ""end"": ""2014-09-08"",
+      ""deleted"": false,
+      ""recurring"": true,
+      ""participation_status"": ""needs_action"",
+      ""transparency"": ""opaque"",
+      ""event_status"": ""confirmed"",
+      ""categories"": [],
+      ""attendees"": [
+        {{
+          ""email"": ""example@cronofy.com"",
+          ""display_name"": ""Example Person"",
+          ""status"": ""needs_action""
+        }}
+      ],
+      ""created"": ""2014-09-01T08:00:01Z"",
+      ""updated"": ""2014-09-01T09:24:16Z"",
+      ""options"": {{
+        ""delete"": true,
+        ""update"": true
+      }},
+      ""event_private"": {0}
+    }}
+  ]
+}}", eventPrivate.ToString().ToLower()))
+            );
+
+            var events = Client.GetEvents();
+
+            CollectionAssert.AreEqual(
+                new List<Event> {
+                    new Event {
+                        CalendarId = "cal_U9uuErStTG@EAAAB_IsAsykA2DBTWqQTf-f0kJw",
+                        EventUid = "evt_external_54008b1a4a41730f8d5c6037",
+                        Summary = "Company Retreat",
+                        Description = "Escape to the country",
+                        Start = new EventTime(new Date(2014, 9, 6), "Etc/UTC"),
+                        End = new EventTime(new Date(2014, 9, 8), "Etc/UTC"),
+                        Location = null,
+                        Deleted = false,
+                        Recurring = true,
+                        ParticipationStatus = AttendeeStatus.NeedsAction,
+                        Transparency = Transparency.Opaque,
+                        EventStatus = EventStatus.Confirmed,
+                        Categories = new string[] {},
+                        Created = new DateTime(2014, 9, 1, 8, 0, 1, DateTimeKind.Utc),
+                        Updated = new DateTime(2014, 9, 1, 9, 24, 16, DateTimeKind.Utc),
+                        Attendees = new[] {
+                            new Attendee {
+                                Email = "example@cronofy.com",
+                                DisplayName = "Example Person",
+                                Status = AttendeeStatus.NeedsAction,
+                            }
+                        },
+                        Options = new EventOptions()
+                        {
+                            Delete = true,
+                            Update = true
+                        },
+                        EventPrivate = eventPrivate,
+                    }
+                },
+                events);
         }
 
         private void AssertParameter(string keyValue, Action<GetEventsRequestBuilder> builderAction)
